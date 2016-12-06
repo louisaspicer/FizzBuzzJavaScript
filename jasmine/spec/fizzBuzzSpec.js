56ea8ea35511a1b3264a3cbe08340f95b864d643
Louisa <louisaspicer@gmail.com>
2016-12-06 12:53:41 +0000

    Added first two fizzBuzz tests

---
+++ jasmine/spec/fizzBuzzSpec.js
@@ -0,0 +1,23 @@
+describe('FizzBuzz', function() {
+
+  var fizzbuzz;
+
+  describe('knows when a number is', function() {
+
+     it('divisible by 3', function() {
+       fizzbuzz = new FizzBuzz();
+       expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
+     });
+
+  });
+
+  describe('knows when a number is NOT', function(){
+
+    it('divisible by 3', function() {
+      fizzbuzz = new FizzBuzz();
+      expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
+    });
+
+  });
+
+});
